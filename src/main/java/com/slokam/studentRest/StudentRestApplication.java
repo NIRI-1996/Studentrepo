72c2c1136745e2ba0d5bbae041d2c4407addd631
NIRI-1996 <gnr.gtr96@gmail.com>
2021-08-20 12:49:51 +0530

    Third commit

--- src/main/java/com/slokam/studentRest/StudentRestApplication.java
+++ src/main/java/com/slokam/studentRest/StudentRestApplication.java
@@ -2,8 +2,10 @@ package com.slokam.studentRest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.slokam.studentRest.Controller"})
 public class StudentRestApplication {
 
 	public static void main(String[] args) {
